b48520498a65d385ea43829f64bddc27c07c9fe7
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-11-12 15:25:11 -0600

    add-services.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import * as headerService from "./services/header/header.service";
+import * as landingServicesService from "./services/landingServices/landingServices.service";
+
 const text = "Welcome to Ionic Motors";
     const titleElement = document.getElementById("title");
     let index = 0;
@@ -11,3 +14,52 @@ const text = "Welcome to Ionic Motors";
     }
 
     window.onload = typeWriter;
+    
+    addEventListener("load", (event) => {
+        const spanResponse = document.getElementById('json_response');
+        const spanResponseId = document.getElementById('json_responseId');
+    
+        userService.getAll()
+        .then(res=>res.json())
+        .then(users=>{
+            console.log(users);
+            spanResponse.innerHTML = JSON.stringify(users);
+        })
+    
+        userService.getById(3)
+        .then(res=>res.json())
+        .then(user=>{
+            console.log(user);
+            //spanResponseId.innerHTML = JSON.stringify(user);
+    
+            // Prueba
+            // Extraer solo los atributos que necesitamos
+            const { firstName, lastName, age } = user;
+    
+            // Crear un nuevo objeto con los atributos seleccionados
+            const datosFiltrados = { firstName, lastName, age };
+    
+            // Mostrar el resultado en la consola o utilizarlo en el DOM
+            console.log(datosFiltrados); // { nombre: "Juan", edad: 25 }
+    
+            // Ejemplo: Mostrar en HTML
+            document.getElementById("nombre-usuario").innerHTML = datosFiltrados.firstName;
+            document.getElementById("nombre-usuario").innerHTML = datosFiltrados.lastName;
+            document.getElementById("edad-usuario").innerHTML = datosFiltrados.age;
+        })
+    
+        const form = document.getElementById('frmUser');
+        form.addEventListener('submit', (event)=>{
+            
+            var formData = new FormData(form);
+            const FormProps = Object.fromEntries(formData);
+    
+            console.log(FormProps);
+            userService.update(FormProps);
+    
+            event.preventDefault();
+    
+        })
+    
+        
+    });
---
+++ js/services/header/header.service.js
@@ -0,0 +1,36 @@
+const baseUrl = 'http://localhost:3000/users';
+
+export const getAll = () =>{
+    return fetch(`${baseUrl}`);
+}
+
+export const getById = (id) =>{
+    return fetch(`${baseUrl}/${id}`);
+}
+
+export const create = (user) =>{
+    return fetch(baseUrl, {
+        method: 'POST',
+        
+        body: JSON.stringify(user),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+}
+
+export const update = (user) =>{
+    fetch(`${baseUrl}/3`, {
+        method: 'PUT',
+
+        body: JSON.stringify(user),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+}
+
+export const remove = (user) =>{
+
+}
+
