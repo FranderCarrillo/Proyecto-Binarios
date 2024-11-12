14acbc8828628eb7b75083bcf2c92fee6fa2a7a7
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-11-11 19:35:47 -0600

    part2-proyecto

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const text = "Bienvenidos a Ionic Motors";
+    const titleElement = document.getElementById("title");
+    let index = 0;
+
+    function typeWriter() {
+        if (index < text.length) {
+            titleElement.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(typeWriter, 100); // ajusta el tiempo para cambiar la velocidad de escritura
+        }
+    }
+
+    window.onload = typeWriter;
