8fd502b8b887a0e3f123e4ea0b86e1b067588fea
FranderCarrillo <frander.carrillo.torres@est.una.ac.cr>
2024-11-12 19:27:22 -0600

    cambios

--- js/services/header/header.service.js
+++ js/services/header/header.service.js
@@ -1,36 +1,45 @@
-const baseUrl = 'http://localhost:3000/users';
+const baseUrl = 'http://localhost:3000/header';
 
-export const getAll = () =>{
-    return fetch(`${baseUrl}`);
+export const getAll = () => {
+ return fetch(`${baseUrl}`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+ });
 }
 
-export const getById = (id) =>{
-    return fetch(`${baseUrl}/${id}`);
+export const getById = (id) => {
+ return fetch(`${baseUrl}/${id}`);
 }
 
-export const create = (user) =>{
-    return fetch(baseUrl, {
+export const create = (header) => {
+    return fetch(`${baseUrl}`, {
         method: 'POST',
-        
-        body: JSON.stringify(user),
+        body: JSON.stringify(header),
         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
+          'Content-type': 'application/json; charset=UTF-8',
         },
-    })
+      });
 }
 
-export const update = (user) =>{
-    fetch(`${baseUrl}/3`, {
+export const update = (id, header) => {
+    return fetch(`${baseUrl}/${id}`, {
         method: 'PUT',
-
-        body: JSON.stringify(user),
+        body: JSON.stringify(header),
         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
+          'Content-type': 'application/json; charset=UTF-8',
         },
-    })
+    });
 }
 
-export const remove = (user) =>{
-
+export const remove = (id) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
 }
+
 
